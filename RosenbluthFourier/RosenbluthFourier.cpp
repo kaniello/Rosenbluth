8a90cdfb1608365af73bafe50c202cf07b1e360a
danilodandrea_vs <danilodandrea_vs@hotmail.com>
2017-07-13 23:24:16 +0200

    Inverse Transform numerically not successfull

--- RosenbluthFourier/RosenbluthFourier.cpp
+++ RosenbluthFourier/RosenbluthFourier.cpp
@@ -49,8 +49,8 @@ int main() {
 	for (k = 0; k < N; k++) {
 		// calc pi*w
 		f[k] = Pi*k*Df;
-		//Yt[k] = 2.*f[k] * double(sqrt(Pi)) / (pow(f[k],2)+pow(a,2));
-		Yt[k] = (1. / 2.)*Pi*f[k] * exp(-pow(f[k] / 2., 2));
+		//Yt[k] = (1. / 2.)*Pi*f[k] * exp(-pow(f[k] / 2., 2));
+		Yt[k] = (1. / 2.)*Pi/(f[k]) * exp(-pow(f[k] / 2., 2));
 		//cout << "f[" << k << "] = " << f[k] << "  Yt[" << k << "] = " << Yt[k] << endl;
 		theory << f[k] << " " << Yt[k] << endl;
 	}
@@ -63,28 +63,31 @@ int main() {
 
 	//cout << "f[0] = 0  Yt[0] = " << Yt[N] * T * double(sqrt(Pi)) << endl;
 
-	for (k = N; k > 1; k--) {
-		Yt[k - 1] = Yt[k - 2] * T * double(sqrt(Pi));
+	for (k = N-1; k >= 1; k--) {
+		//cout << k << "   " <<  Yt[k] << "  " << Yt[k - 1] << endl;
+		Yt[k] = Yt[k - 1] * T * double(sqrt(Pi)) / (f[k]* f[k]);
 		//cout << "f[" << k << "] = " << f[k] << "  Yt[" << k << "] = " << Yt[k-1] << endl;
 	}
+	
+	
 
 	results << 0 << " " << 0 << endl;
-	for (k = 1; k < N - 1; k++) {
-		results << f[k - 1] << " " << Yt[k - 1] << endl;
+	for (k = 1; k <= N - 1; k++) {
+		results << f[k] << " " << Yt[k]  << endl;
 	}
 
-	*Yt = *Yt / pow(*f,2);
-	Yt[0] =  Yt[2] + (f[0] - f[2])*(Yt[1] - Yt[2]) / (f[1] - f[2]);
-	for (k = 1; k < N - 1; k++) {
-		Test << k*T << " " << Yt[k - 1] << endl;
+	//*Yt = *Yt / pow(*f,2);
+	//Yt[0] =  Yt[2] + (f[0] - f[2])*(Yt[1] - Yt[2]) / (f[1] - f[2]);
+	//for (k = 1; k < N - 1; k++) {
+		//Test << k*T << " " << Yt[k - 1] << endl;
 		//cout << "f[" << k << "] = " << f[k] << "  Yt[" << k << "] = " << Yt[k - 1] << endl;
-	}
+	//}
 	
 	p = fftw_plan_r2r_1d(N, Yt, Y, FFTW_RODFT00, FFTW_ESTIMATE);
 	fftw_execute(p);
 
 	for (k = 1; k < N - 1; k++) {
-		//Test << k*T << " " << Y[k - 1] << endl;
+		Test << k*T << " " << Y[k - 1] << endl;
 	}
 		
 	fftw_destroy_plan(p);
